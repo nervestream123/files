bfcd063bf9d8b4511c9c69b027d2b9d80266e5a2
nervestream123 <nervestream123@icloud.com>
2025-07-12 15:42:09 -0500

    updating tasks styling

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -6,15 +6,15 @@ function Tasks() {
     <Flex width="100%" direction="column" className="mb-[10px]">
       <Flex width="100%" className="items-center mb-[5px]">
         <CheckIcon></CheckIcon>
-        <Text size="2" weight="bold">Create new folder called "Archive"</Text>
+        <Text size="1" weight="bold" className="pl-[2px]">Create new folder called "Archive"</Text>
       </Flex>
       <Flex width="100%" className="items-center mb-[5px]">
         <Spinner size="1" />
-        <Text size="2" weight="bold" className="pl-[3px]">Move files from Documents into Archive</Text>
+        <Text size="1" weight="bold" className="pl-[5px]">Move files from Documents into Archive</Text>
       </Flex>
       <Flex width="100%" className="items-center mb-[5px]">
         <DotFilledIcon></DotFilledIcon>
-        <Text size="2" weight="bold" className="pl-[3px]">Lock Archive Folder</Text>
+        <Text size="1" weight="bold" className="pl-[3px]">Lock Archive Folder</Text>
       </Flex>
     </Flex>
   );
